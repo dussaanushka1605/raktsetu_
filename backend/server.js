987541916bb9df7ef78206d09b5e189321824b48
dussaanushka1605 <anushkadussa@gmail.com>
2025-07-18 20:58:16 +0530

    Add new Netlify domain to CORS

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(cors({
         'http://127.0.0.1:8080',
         'http://localhost:5173',
         'http://localhost:8081',
-        'https://raktsetuu.netlify.app' 
+        'https://raktsetuu.netlify.app' ,
+        'https://raktsetu11.netlify.app'
         
     ],
     credentials: true,
